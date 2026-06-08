9df15f635ed0e8e625d4692ac4850609091aec7a
kryso <kryso@users.noreply.github.com>
2026-06-08 23:12:51 +0900

    Rename game title

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var BUILD_VERSION = '20260608.34';
+var BUILD_VERSION = '20260608.35';
 var strSyncingFs = 'Syncing FS...';
 var strDone = 'Done.';
 var strDeleting = 'Deleting...';
@@ -374,7 +374,7 @@ var Module = {
     print: function(text) { console.log(text); },
     printErr: function(text) { console.error(text); },
     locateFile: function(path) {
-        return path === 'sdlpal.wasm' ? 'sdlpal.wasm?v=20260608.34' : path;
+        return path === 'sdlpal.wasm' ? 'sdlpal.wasm?v=20260608.35' : path;
     },
     canvas: (function() {
         var canvas = document.getElementById('canvas');
